0118c3785ea3ae4473904cf6b9a72fae371a83d0
PriscilaBrandao <priscilabrandao1987@gmail.com>
2018-12-21 10:27:45 -0200

    segundo commit criando classe Cliente

---
+++ src/com/fieb/senai/entidades/Cliente.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fieb.senai.entidades;
+
+import com.fieb.senai.enums.Sexo;
+import java.util.Objects;
+
+/**
+ *
+ * @author Casas Bahia
+ */
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+    private Integer idade;
+    private Sexo sexo;
+
+    public Cliente(Integer id, String nome, Integer idade, Sexo sexo) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.nome);
+        hash = 29 * hash + Objects.hashCode(this.idade);
+        hash = 29 * hash + Objects.hashCode(this.sexo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.idade, other.idade)) {
+            return false;
+        }
+        if (this.sexo != other.sexo) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+}
--- src/com/fieb/senai/enums/Sexo.java
+++ src/com/fieb/senai/enums/Sexo.java
@@ -3,12 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.fieb.senai.app;
+package com.fieb.senai.enums;
 
 /**
  *
  * @author Casas Bahia
  */
-public class Avaliacao {
+public enum Sexo {
+    
+    Masculino, 
+    Feminino;
     
 }
